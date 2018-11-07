859c1faca7afc3f359583894e73b25709572d5d7
xsaiter <xsaiter@mail.ru>
2018-11-07 16:32:05 +0300

    improve

--- src/main/lia/JacobiMethod.java
+++ src/main/lia/JacobiMethod.java
@@ -8,35 +8,33 @@ public class JacobiMethod implements LinearSystem.SolveStrategy {
     public Vector solve(Matrix a, Vector b) {
         final int n = b.length();
 
-        Vector res = new Vector(n);
+        var res = new Vector(n);
         res.setAll(1.0);
 
-        Vector buf = new Vector(n);
+        var buf = new Vector(n);
 
-        double norm;
+        double e;
 
         do {
             for (int i = 0; i < n; ++i) {
                 buf.set(i, b.get(i));
-
                 for (int j = 0; j < n; j++) {
                     if (i != j) {
                         buf.setToSub(i, a.get(i, j) * res.get(j));
                     }
                 }
-
                 buf.setToDiv(i, a.get(i, i));
             }
 
-            norm = Math.abs(res.get(0) - buf.get(0));
+            e = Math.abs(res.get(0) - buf.get(0));
 
             for (int i = 0; i < n; ++i) {
-                if (Math.abs(res.get(i) - buf.get(i)) > norm) {
-                    norm = Math.abs(res.get(i) - buf.get(i));
+                if (Math.abs(res.get(i) - buf.get(i)) > e) {
+                    e = Math.abs(res.get(i) - buf.get(i));
                 }
                 res.set(i, buf.get(i));
             }
-        } while (norm > Helper.EPSILON);
+        } while (e > Helper.EPSILON);
 
         return res;
     }
